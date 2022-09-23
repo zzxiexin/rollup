132299d815c8ed4112e41a79e581ed92399cacf4
xinxin.yuan <xinxin.yuan@trip.com>
2022-09-23 12:31:41 +0800

    rollup

---
+++ bundle.js
@@ -0,0 +1,10 @@
+(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+})((function () { 'use strict';
+
+  var version = "1.0.0";
+
+  console.log(version);
+
+}));
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import { a } from "../module/module";
+import { version } from "../package.json";
+console.log(version);
